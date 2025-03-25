d3d4cf79339476ea0f0a4f6ee61a562feb7d75be
Thomas de la Gorgue de Rosny <159559547+lebabouinvolant@users.noreply.github.com>
2025-03-25 18:31:11 +0100

    Update Connection.py
    Added GTK token support

--- Connection.py
+++ Connection.py
@@ -1,16 +1,35 @@
 import requests
 import json
+import urllib.parse
 
 name = "" #Initialization of global vars
 pswd = ""
 Firsttoken = ""
 id = ""
+userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+GTKCookie = ""
+
+def GetGTKCookie():
+    headersGTK={
+        "User-Agent" : userAgent,
+    }
+    r = requests.get("https://api.ecoledirecte.com/v3/login.awp?gtk=1&v=4.75.0", headers=headersGTK)
+    cookie_string = r.headers["Set-Cookie"]
+    Firsttoken = r.headers["X-Token"]
+    global GTKCookie
+    GTKCookie = cookie_string.split(";")[0]
+    return cookie_string.split(";")[0]
+
 
 def ConnectToEd(username, password):
     global name, pswd, Firsttoken
     name, pswd = username, password
+    GetGTKCookie()
+    print(GTKCookie)
     headersConnection={
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "User-Agent" : userAgent,
+        "Cookie" : GTKCookie,
+        "X-Gtk" : GTKCookie.split("=")[1],
     }
     DataConnection={
         "identifiant":username,
@@ -19,11 +38,16 @@ def ConnectToEd(username, password):
         "uuid":"",
         "fa":[],
     }
-    r= requests.post("https://api.ecoledirecte.com/v3/login.awp", data={"data": json.dumps(DataConnection)}, headers=headersConnection)
+    r= requests.post("https://api.ecoledirecte.com/v3/login.awp?v=4.75.0", data={"data": json.dumps(DataConnection)}, headers=headersConnection)
     data =r.json()
+    print(data)
+    if(data["token"] == ""):
+        return 0 #Return 0 if the connection failed
     headers2={
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-        "X-Token": data["token"]
+        "User-Agent" : userAgent,
+        "X-Token": data["token"],
+        "X-Gtk" : GTKCookie.split("=")[1],
+        "Cookie" : GTKCookie,
     }
     QueryString1={
         "verbe":"get",
@@ -32,6 +56,9 @@ def ConnectToEd(username, password):
     dataConnection2={}
     r2 = requests.post("https://api.ecoledirecte.com/v3/connexion/doubleauth.awp", data={"data": json.dumps(dataConnection2)}, headers=headers2, params=QueryString1)
     data2=r2.json()
+    print(data2)
+    if(r2.headers["X-Token"] == ""):
+        return 0 #Return 0 if the connection failed
     Firsttoken = r2.headers["X-Token"]
     return data2["data"]
 
@@ -45,7 +72,9 @@ def ConnectToEdPart2(answer):
     }
     Headers3={
         "X-Token" : Firsttoken,
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "User-Agent" : userAgent,
+        "X-Gtk" : GTKCookie.split("=")[1],
+        "Cookie" : GTKCookie,
     }
     r3 = requests.post("https://api.ecoledirecte.com/v3/connexion/doubleauth.awp", data={"data": json.dumps(dataConnection3)}, headers=Headers3, params=queryString3)
     data3 = r3.json()
@@ -61,7 +90,10 @@ def ConnectToEdPart2(answer):
     }
     Headers4={
         "X-Token":r3.headers["X-Token"],
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "User-Agent" : userAgent,
+        "X-Gtk" : GTKCookie.split("=")[1],
+        "Cookie" : GTKCookie,
+
     }
     r4 = requests.post("https://api.ecoledirecte.com/v3/login.awp", data={"data": json.dumps(dataConnection4)}, headers=Headers4)
     global id
@@ -75,7 +107,10 @@ def AskForNotes(Token):
     }
     headersNoteRequest={
         "X-Token" : Token,
-        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "User-Agent" : userAgent,
+        "X-Gtk" : GTKCookie.split("=")[1],
+        "Cookie" : GTKCookie,
+
     }
     queryStringNoteRequest={
         "verbe" : "get",
